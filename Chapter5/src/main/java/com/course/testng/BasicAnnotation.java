3dc6c71feff383fcabd342b9ea6f1b261b977be5
aihongye <1776923930@qq.com>
2019-12-19 07:18:00 +0800

    测试用例1

--- Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
+++ Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -1,13 +1,33 @@
 package com.course.testng;
 
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BasicAnnotation {
     //最基本的注解，用来把方法标记成测试的一部分
     @Test
-    public void testCase1(){
-        System.out.println("------------这是测试用例1=========");
-
+    public void testCase1()
+    {
+        System.out.println("这是测试用例1");
+    }
+    @Test
+    public void testCase2(){
+        System.out.println("这是测试用例2");
+    }
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("beforeMethod这是在测试方法之前运行");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("afterMethod这是在测试方法之后运行");
+    }
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("beforeClass这是在类运行之前运行");
+    }
+    @AfterClass
+    public void afterClass(){
+        System.out.println("afterClass这是在类运行之后运行");
     }
 }
