7b2448306d00b724e128e52fc02ed9e0df524f10
aihongye <1776923930@qq.com>
2019-12-27 16:28:12 +0800

    selenuim baidu

--- SelenuimDemo/src/main/java/Demo.java
+++ SelenuimDemo/src/main/java/Demo.java
@@ -1,8 +1,9 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo {
-    public  static  void  main(String[],args) {
-        public WebDriver webdirver;
+    public  static void  main(String[]args) {
+        WebDriver webdriver;
         String driverLoc = "D:\\Program Files\\Apache\\chromedriver.exe";
         String driver = "webdriver.chrome.driver";
         System.setProperty(driver, driverLoc);
