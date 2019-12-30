39e2011de23d3d63c2f9bbbc44687617ae21bc9e
aihongye <1776923930@qq.com>
2019-12-30 22:42:34 +0800

    selenuim demo2 封装方法获取输入框的属性值并打印

--- SelenuimDemo/src/main/java/Demo1.java
+++ SelenuimDemo/src/main/java/Demo1.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 
+/**
+ * 打开慕课网首页，点击各个元素。整体练习
+ */
 public class Demo1 {
     public  static void  main(String[]args) {
         WebDriver webdriver;
@@ -13,16 +16,22 @@ public class Demo1 {
         System.setProperty(driver, driverLoc);
         webdriver = new ChromeDriver();
         //webdriver.get("https://www.baidu.com/");
+        //慕课网登录界面
         webdriver.get("https://www.imooc.com/user/newlogin/from_url");
         webdriver.findElement(By.tagName("input")).sendKeys("13331353667");
         webdriver.findElement(By.name("password")).sendKeys("aihongye99");
         webdriver.findElement(By.id("auto-signin")).click();
-        //webdriver.findElement(By.linkText("注册")).click();
-        // webdriver.findElement(By.partialLinkText("登录")).click();
-        // webdriver.findElement(By.name("password")).sendKeys("aihongye99");
+        //webdriver.findElement(By.linkText("注册")).click();//没有成功定位
+        //webdriver.findElement(By.partialLinkText("登录")).click();//没有成功定位
+        //webdriver.findElement(By.name("password")).sendKeys("aihongye99");//没有成功定位
+        //点击“注册”页卡
+        //webdriver.findElement(By.className("xa-showSignup")).click();
+        //点击“登录”页卡
+        //webdriver.findElement(By.className("active-title")).click();
+
         webdriver.findElement(By.className("moco-btn-red")).click();
         webdriver.manage().window().maximize();
-        webdriver.get("https://www.imooc.com");
+        //webdriver.get("https://www.imooc.com");
         webdriver.findElement(By.className("search-input")).click();
         webdriver.findElement(By.className("search-input")).sendKeys("sss");
         WebElement element = webdriver.findElement(By.className("nav-item"));
---
+++ SelenuimDemo/src/main/java/Demo2.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * 封装方法
+ */
+public class Demo2 {
+    public WebDriver driver;
+    public void InitDriver(){
+        String driverLoc = "D:\\Program Files\\Apache\\chromedriver.exe";
+        String webdriver = "webdriver.chrome.driver";
+        System.setProperty(webdriver, driverLoc);
+        driver = new ChromeDriver();
+        //慕课网登录界面
+        driver.get("https://www.imooc.com/user/newlogin/from_url");
+        driver.manage().window().maximize();
+    }
+    public void inputBox(){
+        driver.findElement(By.name("email")).sendKeys("13331353667");
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.name("email")).clear();
+        //获取输入框的某个属性值，并打印
+        String sss=driver.findElement(By.name("email")).getAttribute("placeholder");
+        System.out.println(sss);
+    }
+    public static void main (String[] args){
+        Demo2 demo2 =new Demo2();
+        demo2.InitDriver();;
+        demo2.inputBox();
+    }
+}
