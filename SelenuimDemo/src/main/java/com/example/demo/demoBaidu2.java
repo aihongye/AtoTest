4d2fa92fb5489f8c98068f79b3dd78b284500721
aihongye <1776923930@qq.com>
2020-01-06 17:28:37 +0800

    selenuim 乐搏页面新页面句柄获取

--- SelenuimDemo/src/main/java/com/example/demo/demoBaidu2.java
+++ SelenuimDemo/src/main/java/com/example/demo/demoBaidu2.java
@@ -3,10 +3,9 @@ package com.example.demo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.Iterator;
-import java.util.Set;
-
+/*
+测试新页面句柄的获取
+ */
 public class demoBaidu2 {
     public  static void  main(String[]args){
         WebDriver webdriver;
@@ -35,12 +34,31 @@ public class demoBaidu2 {
             e.printStackTrace();
         }
 
-        //针对跳转后的新网页进行新的对象实例化
-        String newHandle = webdriver.getWindowHandle();
-        Set<String>   Handles = webdriver.getWindowHandles();
+        //获取当前页面句柄
+        String handle = webdriver.getWindowHandle();
+        //获取所有页面句柄，并循环判断是不是当前的句柄就做选switchTo()
+        for (String handles:webdriver.getWindowHandles()){
+             if (handles.equals(handle))
+                 continue;
+             //跳过本次循环，不执行下面的命令直接进行下一次循环
+            //break;//结束全部for循环
+            //当前页面与遍历的页面不一致时，说明获取到了新页面，这是页面跳转到新页面
+            webdriver.switchTo().window(handles);
+        }
+        System.out.println("新页面地址==="+webdriver.getCurrentUrl());
+        if (webdriver.getCurrentUrl().equals("http://www.leboxcap.com/"))
+        {
+            System.out.println("恭喜您，测试用例执行成功了");
+        }
+        else
+        {
+            System.out.println("您的用例没有执行成功");
+        }
+ /*       //这部分代码没有执行成功，可以参考逻辑
+  Set<String>   Handles = webdriver.getWindowHandles();
         Iterator<String> it =Handles.iterator();
         WebDriver driver1;
-        while (it.hasNext()){
+       while (it.hasNext()){
             System.out.println(it.next().toCharArray());
             if (it.next()== newHandle) {
                 System.out.println("1111111111111111");
@@ -53,7 +71,7 @@ public class demoBaidu2 {
 
             }
             continue;
-        }
+        }*/
 
         System.out.println("用例执行完毕！");
         webdriver.close();
